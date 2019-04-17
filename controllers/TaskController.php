a326e6e4ce23b4b8077fad0f430a6a8a01567426
TiysEvgeny <tiys@ngs.ru>
2019-04-18 04:42:48 +0700

    Добавил Yii фреймворк и добавил классы TaskController и AddTask

---
+++ controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tiys
+ * Date: 18.04.2019
+ * Time: 3:41
+ */
+
+namespace app\controllers;
+use yii\web\Controller;
+
+class TaskController extends Controller
+{
+	public function actionAddTask (){
+		return $this->render('addTask')
+	}
+}
---
+++ views/task/addTask.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tiys
+ * Date: 18.04.2019
+ * Time: 4:11
+ */
+<form method=get>
+	Введите, что неоходимо сделать:<br>
+	<input type="text" name="title" >
+
+	<input type=button
+</form>
