9a717c5ad7f8e873ab3f84753c3c21543dcdd209
Evgeny <tiys@ngs.ru>
2019-04-18 16:58:33 +0700

    Добавил view для создания задачи

--- views/task/addTask.php
+++ views/task/addTask.php
@@ -6,8 +6,11 @@
  * Time: 4:11
  */
 <form method=get>
-	Введите, что неоходимо сделать:<br>
-	<input type="text" name="title" >
-
-	<input type=button
+	Введите, что необходимо сделать:<br>
+	<input type="text" name="title">
+	Подробное описание:
+	<textarea></textarea>
+	Кто будет исполнителем:
+	<input type="text" name="responsible">
+	<input type="button" value="Добавить задачу">
 </form>
