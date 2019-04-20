6a3312338c36f91279ef7e5c9ef3c6b99f511737
TiysEvgeny <tiys@ngs.ru>
2019-04-20 14:59:16 +0700

    Создал подобие валидатора, но не удалось протестировать, поскольку пока не ясно каким образом yii получает POST-запрос из формы.

--- validators/ResultOrientedValidator.php
+++ validators/ResultOrientedValidator.php
@@ -2,7 +2,24 @@
 
 use yii\validators\Validator;
 
-class MyValidator extends Validator
+/**
+ * Результат-ориентированная формулировка задач означает, что в случае выполнения этой задачи,
+ * точно будет завершено какое-либо действие, а не будет участие в каком-то процессе,
+ * который целью не является, поэтому желательно, чтобы в формулировке был указан глагол,
+ * но полноценная проверка ориентированности на результат невозможна
+ */
+class ResultOrientedValidator extends Validator
 {
-
+	public $terms=('finish','done','/^\w+ть/','/^\w+ие\W/');
+	/**
+	 * @var array dictionary of words expressing result oriented formulation of tasks.
+	 */
+	public function validateAttribute($model, $attribute)
+	{
+		foreach ($terms as $term){
+			if($attribute!='' && !preg_match($term, $attribute)){
+				$this->addError($model,$attribute, $this->message);
+			}
+		}
+	}
 }
